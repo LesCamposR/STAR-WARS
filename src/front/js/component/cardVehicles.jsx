898264ba21e4fd65c0f7f1cb829d9bb0026e795c
LesCamposR <lesthercamposramirez91@gmail.com>
2023-04-12 17:51:54 +0000

    cards

--- src/front/js/component/cardVehicles.jsx
+++ src/front/js/component/cardVehicles.jsx
@@ -2,29 +2,30 @@ import React, { useState, useEffect, useContext } from "react";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 
-const CardPeople = (props) => {
+const CardVehicle = (props) => {
     const { store, actions } = useContext(Context)
 
     return (
         <div className="card-container d-flex overflow-scroll" style={{ width: "400px" }}>
-            <div className="card m-2">
+            <div className="card m-2 d-flex">
                 <div className="card-body m-1 p-1">
-                    <img className="rounded img-thumbnail img-center" src={"https://starwars-visualguide.com/assets/img/characters/" + item.uid + ".jpg"} />
-                    <h4 className="card-title mt-2 text-center">{props.name}</h4>
+                    <img className="rounded img-thumbnail img-center" src={"https://starwars-visualguide.com/assets/img/vehicles/" + item.uid + ".jpg"} alt="Vehicle Image" />
+                    <br />
+                    <h3 className="card-title mt-2 text-center">{props.name}</h3>
                     <p className="card-text text-start ps-4 mb-2"><em>Information</em>
                         <ul className="text-start ps-4">
-                            <li key={props.uid}>Height: {props.height}</li>
-                            <li key={props.uid}>Mass: {props.mass}</li>
+                            <div key={props.uid}>Model: {props.model}</div>
+                            <div key={props.uid}>Manufacturer: {props.manufacturer}</div>
                         </ul>
                     </p>
                     <div className="text-center">
-                        <Link to={`/people/${props.uid}`} className="btn btn-outline-primary me-5">Learn More!</Link>
+                        <Link to={`/vehicle/${props.uid}`} className="btn btn-outline-primary me-5">Learn More!</Link>
                         <button type="button" onClick={() => {
                             actions.agregarFavorito({
                                 name: props.name,
                                 uid: props.uid,
-                                category: "people",
-                                link: `/people/${props.uid}`
+                                category: "vehicle",
+                                link: `/vehicle/${props.uid}`
                             }
                             )
                         }} className="btn btn-outline-warning ms-5"><i className="far fa-heart"></i></button>
@@ -33,7 +34,6 @@ const CardPeople = (props) => {
             </div>
         </div>
     );
+};
 
-}
-
-export default CardPeople;
+export default CardVehicle;
